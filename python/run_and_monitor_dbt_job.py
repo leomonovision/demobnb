b52fcafc6a5ad69834ca546f3ca54cc1f22bf806
Leo Monot <lmonot@actinvision.com>
2023-11-23 14:22:12 +0000

    revert

--- python/run_and_monitor_dbt_job.py
+++ python/run_and_monitor_dbt_job.py
@@ -7,6 +7,6 @@ job_cause       = os.getenv('DBT_JOB_CAUSE', 'API-triggered job') # default to g
 git_branch      = os.getenv('DBT_JOB_BRANCH', None) # default to None
 schema_override = os.getenv('DBT_JOB_SCHEMA_OVERRIDE', None) # default to None
 api_key         = os.environ['DBT_API_KEY']  # no default here, just throw an error here if key not provided
-account_id      = os.environ[28297] # no default here, just throw an error here if id not provided
+account_id      = os.environ['28297'] # no default here, just throw an error here if id not provided
 project_id      = os.environ['317706'] # no default here, just throw an error here if id not provided
 job_id          = os.environ['461701'] # no default here, just throw an error here if id not provided
